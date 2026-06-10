77b45894b03708d974b02243076a1e67957109bb
Suyog2121 <ainarsuyog553@gmail.com>
2026-06-10 19:38:55 +0530

    Create Program3.py for prime number checking
    Add a Python script to check if a number is prime.

---
+++ Program3.py
@@ -0,0 +1,9 @@
+#check prime number
+num = 3
+if num > 1:
+    for i in range(2, num):
+        if (num % i) == 0:
+            print("Not Prime")
+            break
+    else:
+        print("Not Prime")
