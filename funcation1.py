37d125b6a0736d3df94536050d3433f5912a2eb3
Suyog2121 <ainarsuyog553@gmail.com>
2026-06-11 12:30:34 +0530

    Add banking functions in funcation1.py

---
+++ funcation1.py
@@ -0,0 +1,22 @@
+def welcome():
+    print("Welcome to Bank")
+
+def deposit():
+    print("Amount Deposited:1000")
+
+def withdraw():
+    print("Amount Withdrewn:500")
+
+def balance():
+    print("Balance:5000")
+
+def application():
+    print("Application for ATM Card")
+
+welcome()
+deposit()
+withdraw()
+balance()
+application()
+
+    
