725497ad77ff3123fda598adf465d7e20cf1a4ac
Suyog2121 <ainarsuyog553@gmail.com>
2026-06-11 20:37:47 +0530

    Add bank application form in Bank.py

---
+++ Bank.py
@@ -0,0 +1,19 @@
+print("======== BANK APPLICATIN FORM========")
+
+account_no = 2006
+name = "Suyogainar"
+mobile = "9699304596"
+email = "ainarsuyog553@gmail.com"
+address = "Mandve bk."
+account_type = "Saving"
+balance = 50000.00
+print("\n======== ACCOUNT DETAILS======")
+print("Account Number:",account_no)
+print("Customer Name:",name)
+print("Mobile Number:",mobile)
+print("Email ID:",email)
+print("Address:",address)
+print("Account Type:",account_type)
+print("Balance:",balance)
+
+print("\nAccount Created Successfully!")
