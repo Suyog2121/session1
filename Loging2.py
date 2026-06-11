edebc0233270e6d5063eb832c02b7292a0ef9d11
Suyog2121 <ainarsuyog553@gmail.com>
2026-06-11 20:25:22 +0530

    Add login functionality in Loging2.py

---
+++ Loging2.py
@@ -0,0 +1,9 @@
+username = "suyog"
+password = "553653"
+
+if username == "suyog" and password == "553653":
+    print("Welcome suyog")
+elif username == "admin" and password == "admin123":
+    print("Welcome Admin")
+else:
+    print("Invalid Login")
